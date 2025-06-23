27591c631b28ea13a2a71aa8ca5b79835ab607b5
673498Kingowen <nanthawatbutsri1999@gmail.com>
2025-06-23 12:39:12 +0200

    updated

--- IracingTelemetry/obj/Debug/net9.0-windows/IracingTelemetry.AssemblyInfo.cs
+++ IracingTelemetry/obj/Debug/net9.0-windows/IracingTelemetry.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IracingTelemetry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fae79fd70e69f4afee047605e6f3539040d8548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe53038f4582498dd966d4725fba2831d29fa6b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("IracingTelemetry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IracingTelemetry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
